c6c116c7b67bac965ba732b57958a6a6ddef4b67
Septo Adrianto Azhar <74102464+yuzuru-otonashi@users.noreply.github.com>
2023-12-31 19:54:38 +0700

    Main.jsx

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Video from "./Video/aowkoawoakw.mp4"
+
+const Main = () => {
+    return (
+        <div className="main">
+            <video src={Video} autoPlay loop />
+        </div>
+    )
+}
+
+export default Main
